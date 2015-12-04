e7519adb8dbf127a0aeb2a6e7825cd4410328ae7
clk <clk+git@gamesofa.com>
2015-12-04 13:58:39 +0800

    modify links to zh-wikipedia entries to link to zh-tw pages

--- user_scripts/zh.wikipedia.org.user.js
+++ user_scripts/zh.wikipedia.org.user.js
@@ -7,12 +7,37 @@
 // @grant       none
 // ==/UserScript==
 
-(function(){
+(function() {
     var path = location.pathname;
-    if (-1 < path.indexOf('/zh-tw/'))
-        return;
+    if (0 >  path.indexOf('/zh-tw/') && !path.endsWith('.php')) {
+        path = path.substring(1).split('/');
+        path[0] = '/zh-tw';
+        location.href = path.join('/');
 
-    path = path.substring(1).split('/');
-    path[0] = '/zh-tw';
-    location.href = path.join('/');
+        return;
+    }
+
+    var anchors = document.getElementsByTagName('a');
+    for (var i = 0 ; i < anchors.length ; ++i) {
+        var a = anchors[i];
+        var url = a.href;
+
+        var match = /(?:(https?:)?)\/\/(zh\.wikipedia\.org)\/([^\/]+)\/(.*)/.exec(url);
+        if (!match)
+            continue;
+
+        var protocol, host, variant, path;
+        protocol = match[1];
+        host = match[2];
+        variant = match[3];
+        path = match[4];
+
+        if ('zh-tw' === variant)
+            continue;
+
+        if (/^[A-Z][A-Za-z_]*:/.test(path))
+           continue;
+
+        a.href = protocol + '//' + host + '/zh-tw/' + path;
+    }
 })();
